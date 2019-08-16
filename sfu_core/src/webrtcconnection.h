8793c233e269acf9e5ba252bca1b461706a9c6e7
build <build@ics.com>
2019-08-16 09:05:37 +0800

    add webrtcconnection

---
+++ sfu_core/src/webrtcconnection.h
@@ -0,0 +1,17 @@
+#ifndef _WEB_RTC_CONNECTION_H__
+#define _WEB_RTC_CONNECTION_H_
+
+namespace sfup{
+	namespace sfupconn{
+		class WebrtcConnection{
+			public:
+				WebrtcConnection();
+				virtual ~WebrtcConnection();
+
+
+		};
+	}
+
+}
+
+#endif
