b8cc2ce3061f03dc832edc88651fd87e92809aa2
build <build@ics.com>
2019-08-20 11:41:39 +0800

    update webrtc connection

--- sfu_core/src/webrtcconnection.h
+++ sfu_core/src/webrtcconnection.h
@@ -1,6 +1,16 @@
 #ifndef _WEB_RTC_CONNECTION_H__
 #define _WEB_RTC_CONNECTION_H_
 
+//负责与webrtc客户端交互与媒体流的转发
+/*
+	web client --->> createOffer --->> fake webrtc module
+	fake webrtc module --->> createAnswer --->> web client
+	web client --->> mediastream --->> fake webrtc module
+	fake webrtc module --->> send media stream to peer
+*/
+
+#include <iostream>
+
 namespace sfup{
 	namespace sfupconn{
 		class WebrtcConnection{
@@ -8,7 +18,10 @@ namespace sfup{
 				WebrtcConnection();
 				virtual ~WebrtcConnection();
 
-
+				std::string createOffer();
+				std::string createAnswer();
+				
+				
 		};
 	}
 
